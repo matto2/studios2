cd72587b251fa0e0379f88a1d1795efc13f69760
Matthew O'Leary <mattoleary@Matthews-MacBook-Pro.local>
2017-12-23 09:36:55 -0800

    first commit

---
+++ bookcovers.php
@@ -0,0 +1,148 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>O'Leary Studios | Welcome</title>
+    <link rel="stylesheet" href="css/foundation.css"  type='text/css'/>
+    <script src="js/vendor/modernizr.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+
+
+    <div class="row">
+    <div class="large-12 columns">
+ 
+     
+ 
+      <div class="row">
+        <div class="large-12 columns">
+ 
+          <nav class="top-bar" data-topbar>
+            <ul class="title-area">
+               
+              <li class="name">
+                <h1>
+                  <a href="index.php">
+                    O'Leary Studios
+                  </a>
+                </h1>
+              </li>
+              <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+            </ul>
+         
+            <section class="top-bar-section">
+               
+              <ul class="right">                
+                <li class="divider"></li>
+                <li><a href="about.php">About</a></li>
+                <li class="divider"></li>
+              </ul>
+            </section>
+          </nav>
+           
+        </div>
+      </div>
+ 
+     
+ 
+      <div class="row">
+ 
+   
+ 
+        <div class="large-4 small-12 columns">
+ 
+ <!--
+          <img src="img/me-surfing-2014-500px.jpg">
+ 
+ -->
+          <div>
+            <h3>Book covers</h3>
+            <h5 class="subheader">Selected cover designs created for FastPencil and Premiere imprints.
+            </h5>
+            <small>
+            <a href="index.php">home</a> | <a href="logos.php">Next Category</a>
+            </small>
+
+          </div>
+ 
+ 
+        </div>
+ 
+     
+ 
+ 
+     
+ 
+        <div class="large-8 columns">
+          <ul data-orbit>
+              <li><img src="img/torgue_6x9_iTunes.jpg"  style="max-height:377px;" /></li>
+              <li><img src="img/500miles1.jpg"  style="max-height:377px;" /></li>
+              <li><img src="img/Homework-Cover_forWeb1.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/kills1.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/TOGETHER-THEY-HOLD-UP-THE-SKY_550.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/shakespeare1.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/conversations1.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/Gullibills_Travels1.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/T2T-CoverSpread3c1.jpg"  style="max-height:377px;"/></li> 
+              <li><img src="img/33000_cover_front.jpg" style="max-height:377px;" /></li>                           
+              <li><img src="img/WerePrego.jpg"  style="max-height:377px;"/></li>
+              <li><img src="img/NEW-healer-Cov-4_newFont-1.jpg"  style="max-height:377px;"/></li>
+
+
+
+            </ul>
+ 
+
+ 
+           
+        </div>
+        
+        
+ 
+     
+ 
+ 
+     
+          
+     
+ 
+          </div>
+        </div>
+      </div>
+ 
+ 
+     
+ 
+      <footer class="row">
+        <div class="large-12 columns"><hr/>
+          <div class="row">
+ 
+            <div class="large-6 columns">
+              <small>&copy; Copyright 2017 Matt O'Leary<?php echo date('Y') ?</small>
+            </div>
+ 
+            <div class="large-6 columns">
+              <ul class="inline-list right">
+                <li><a href="about.php"></a></li>
+
+              </ul>
+            </div>
+ 
+          </div>
+        </div>
+      </footer>
+ 
+     
+ 
+    </div>
+  </div>
+
+   <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+      </body>
+</html>
---
+++ contactthanks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	
+</head>
+
+<body>
+
+	<div id="page-wrap">
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="about.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+
+</body>
+
+</html>
